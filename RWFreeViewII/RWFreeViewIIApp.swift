562d76696a362e870998d49a01ba660fd953306a
hajinopro <hajinopro@gmail.com>
2022-04-16 12:25:22 +0900

    Initial Commit

---
+++ RWFreeViewII/RWFreeViewIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RWFreeViewIIApp.swift
+//  RWFreeViewII
+//
+//  Created by 하진호 on 2022/04/16.
+//
+
+import SwiftUI
+
+@main
+struct RWFreeViewIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
